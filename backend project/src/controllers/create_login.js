7f3c4cac28a26697943e7d6dbbdf5cecd07177ad
Kiatiyos-B <kiatiyos.bb@gmail.com>
2024-09-05 23:14:47 +0700

    update add API Login

--- backend project/src/controllers/create_login.js
+++ backend project/src/controllers/create_login.js
@@ -1,6 +1,8 @@
 const bcrypt = require('bcrypt');
 const connection = require('../config/database');
 
+
+// ระบบ signup //
 const createUser = async (req, res) => {
     const { username, password, email, display_name, profile_img, role, total_score, certificate_name } = req.body;
 
@@ -32,7 +34,56 @@ const createUser = async (req, res) => {
 };
 
 
+
+
+// ระบบ login //
+const loginUser = async (req, res) => {
+    const { username, password } = req.body;
+
+    // ตรวจสอบว่าข้อมูลจำเป็นถูกส่งมาหรือไม่
+    if (!username || !password) {
+        return res.status(400).json({ message: 'Username and password are required.' });
+    }
+
+    try {
+        // ค้นหาผู้ใช้ในฐานข้อมูล
+        connection.query(
+            "SELECT * FROM users WHERE username = ?",
+            [username],
+            async (err, results, fields) => {
+                if (err) {
+                    console.error("Error querying the database", err);
+                    return res.status(500).json({ message: 'Error processing request.' });
+                }
+
+                if (results.length === 0) {
+                    // ถ้าไม่พบผู้ใช้
+                    return res.status(401).json({ message: 'Invalid username or password.' });
+                }
+
+                const user = results[0];
+
+                // เปรียบเทียบรหัสผ่านที่ถูกแฮช
+                const passwordMatch = await bcrypt.compare(password, user.password);
+
+                if (!passwordMatch) {
+                    // ถ้ารหัสผ่านไม่ตรง
+                    return res.status(401).json({ message: 'Invalid username or password.' });
+                }
+
+                // หากรหัสผ่านถูกต้อง
+                return res.status(200).json({ message: 'Login successful', user });
+            }
+        );
+    } catch (error) {
+        console.error("Error comparing password", error);
+        return res.status(500).json({ message: 'Error processing request.' });
+    }
+};
+
+
 module.exports = {
     createUser,
+    loginUser
    
 };
--- backend project/src/routes/userRoutes.js
+++ backend project/src/routes/userRoutes.js
@@ -6,6 +6,9 @@ const test = require('../controllers/test')
 // เส้นทางสำหรับสร้างผู้ใช้ใหม่ /users/create
 router.post('/create', create_login.createUser);
 
+// เส้นทางสำหรับสร้างผู้ใช้ใหม่ /users/create
+router.post('/login', create_login.loginUser);
+
 // เส้นทางสำหรับ /users/test ที่เรียกใช้ฟังก์ชัน testApi
 router.get('/test', test.testApi);
 
